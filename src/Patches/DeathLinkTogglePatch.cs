75fd8155048cdb9a037c7bc9b2de4a91fc4cc484
nepcore <nepcore@griem.xyz>
2026-09-16 16:09:22 +0200

    testing around

--- src/Patches/DeathLinkTogglePatch.cs
+++ src/Patches/DeathLinkTogglePatch.cs
@@ -24,3 +24,30 @@ public class DeathLinkTogglePatch : IRwbyGameplayPatch
         btn.Label.text = $"DEATH LINK: {(RWBYAP.Connection.IsDeathLinkOn() ? "ON" : "OFF")}";
     }
 }
+
+// prestige challenge 1: 4 characters level 10
+[HarmonyPatch(typeof(PreGameLobbyController), "Initialize")]
+public class PrestigePatch : IRwbyGameplayPatch
+{
+    public static void Postfix(GenericSelectionItem ___m_privacyButton)
+    {
+        ___m_privacyButton.Button.onClick = new();
+        if (___m_privacyButton == null) return;
+        ___m_privacyButton.Label.text = "RANK UP";
+        ___m_privacyButton.Button.onClick.AddListener(() => {
+            var am = Roost.Singleton_MonoBehaviour<ApplicationManager>.Instance;
+            var m = am.GetType().GetMethod("ChangeLocalState", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var currState = am.State;
+            var actions = new PrestigePanelController.Actions();
+            UnityEngine.Events.UnityAction back = () => {
+                m.Invoke(am, [currState]);
+            };
+            actions.OnBack = back;
+            actions.OnPrestigeLevelUp = () => {
+                am.Profile.IncreasePrestigeLevel();
+                m.Invoke(am, [new GameState_PrestigeAward(back)]);
+            };
+            m.Invoke(am, [new GameState_PrestigePanel(actions)]);
+        });
+    }
+}
